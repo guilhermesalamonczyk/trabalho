043ff1c787b6c3081b4797da058a813b348f6a83
gui <guilherme.salamonczyk@escola.pr.gov.br>
2026-03-11 09:52:13 -0300

    Add files via upload

---
+++ 3E/calculadora.py
@@ -0,0 +1,25 @@
+#calculadora de Aprovação Escolar 
+
+nome = input("Digite o nome do estudante:")
+
+soma_notas = 0
+quantidade_trimestres = 3
+meta_aprovaçao = 180
+
+for i range( 1, 
+            quantidade_trimestre + 1 ):
+    nota = float(input("informe a nota {1}º periodo:"))
+soma_notas += notas 
+
+print("-" * 30)
+print( f"estudante: {nome}")
+print(f"Pontuaçao total : {soma_notas}")
+
+#Verificar o status de aprovaçao
+if soma_notas >= meta_aprovaçao:
+    print(status : APROVADAO! parabens!!)
+else: 
+ pontos_falantes = 
+ meta aprovaçao - soma_notas 
+ print("status: TENTE OUTRA VEZ!") 
+ print(f"faltaram {pontos-}" )
